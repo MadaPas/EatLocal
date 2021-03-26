890a138d23247805cade93bccaf1f9cb5b261c81
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-03-26 13:56:12 +0100

    initial backend setup

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+require('dotenv').config(); 
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Our API is running...');
+  });
+
+  app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`));
