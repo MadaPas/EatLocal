4440e0c8887d2a70080009fe57098cfc3252c2ce
anastasia-pon <daedelu@gmail.com>
2021-03-31 11:39:46 +0200

    delete unnecesarry files

--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const userRoutes = require('./userRoute.js');
-
-const router = express.Router();
-
-router.use('/users', userRoutes);
-
-module.exports = router;
--- server/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-const { getAllUsers, getUser, registerUser } = require('../controllers/user');
-
-router.get('/', getAllUsers);
-router.post('/user', getUser);
-router.post('/register', registerUser);
-module.exports = router;
