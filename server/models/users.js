5bfec263bc0302d7a6fbb3919e0cec891009b6b2
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-04-01 16:14:12 +0200

    changed to camel case

--- server/models/users.js
+++ server/models/users.js
@@ -2,18 +2,22 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
   {
-    okta_id: {
+    oktaId: {
       type: String,
+      required: true,
     },
-    first_name: {
+    firstName: {
       type: String,
+      required: true,
     },
-    last_name: {
+    lastName: {
       type: String,
+      required: true,
     },
     email: {
       type: String,
       unique: true,
+      required: true,
     },
     address: {
       street: {
