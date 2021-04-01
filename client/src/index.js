04ecb93eefaaab32131605d53652dd4b043195f4
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-04-01 16:13:25 +0200

    removed context - moved to app

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './polyfills';
-import { GeneralProvider } from './context/General';
+
 import App from './App';
+
 import './assets/styles/app.scss';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <GeneralProvider>
-    <Router>
-      <App />
-    </Router>
-  </GeneralProvider>, document.getElementById('root'),
+  <Router>
+    <App />
+  </Router>, document.getElementById('root'),
 );
 registerServiceWorker();
