7e29613e2a9dd438014d04dfeeed8f09c57f08a8
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-04-01 19:18:41 +0200

    update user controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,6 @@
-/* eslint-disable camelcase */
-const { User } = require('../models/users');
+const {
+  User,
+} = require('../models/users');
 
 const getAllUsers = async (req, res) => {
   await User.find({}, (err, users) => {
@@ -12,7 +13,9 @@ const getUser = (req, res) => {
   if (!req.body.oktaId.match(/^[0-9a-zA-Z]{10,30}$/)) {
     return res.status(404).json('Wrong user id format. Try again.');
   }
-  return User.find({ oktaId: req.body.oktaId }, (err, user) => {
+  return User.find({
+    oktaId: req.body.oktaId,
+  }, (err, user) => {
     if (!user || user.length === 0) {
       return res.status(404).json('No user found. Try again.');
     }
@@ -25,23 +28,59 @@ const getUser = (req, res) => {
 
 const registerUser = async (req, res) => {
   const {
-    oktaId, email, firstName, lastName,
+    oktaId,
+    email,
+    firstName,
+    lastName,
   } = req.body;
 
-  const userExists = await User.findOne({ oktaId });
+  const userExists = await User.findOne({
+    oktaId,
+  });
 
   if (userExists) {
     return res.status(400).json('This user already exists');
-    // throw new Error('User already exists.');
   }
 
   const user = await User.create({
-    oktaId, email, firstName, lastName,
+    oktaId,
+    email,
+    firstName,
+    lastName,
   });
 
   return res.status(201).json(user);
 };
 
+const updateUser = async (req, res) => {
+  const {
+    oktaId,
+    street,
+    postalCode,
+    city,
+  } = req.body;
+
+  try {
+    const user = await User.findOne({
+      oktaId,
+    });
+
+    if (!user) {
+      res.status(404).json('User not found');
+    }
+
+    user.address.street = street;
+    user.address.postalCode = postalCode;
+    user.address.city = city;
+
+    const updatedUser = await user.save();
+    res.json(updatedUser);
+  } catch (err) {
+    res.status(500).json('Internal server error');
+  }
+};
+
 module.exports.getAllUsers = getAllUsers;
 module.exports.getUser = getUser;
 module.exports.registerUser = registerUser;
+module.exports.updateUser = updateUser;
