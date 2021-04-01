eb4286f3989116aefac2102466173945741e0b0f
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-04-01 14:42:43 +0200

    comm

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -11,41 +11,21 @@ const Checkout = () => {
   }
   const box = allBoxes.filter(b => b._id === order[0]);
   return (
-    <article key={box[0]._id} className="box">
+    <div key={box[0]._id} className="box">
       <h1>Checkout</h1>
       <p>Call to place an order: 1234567890</p>
       <p>You ordered:</p>
       <img src={box[0].img} alt="place holder" />
       <p className="box__name">{box[0].name}</p>
       <p className="box__type">{box[0].type}</p>
-      <p className="box__desc">{box[0].description}</p>
-      <div className="box__menus">
-        <div className="box__menu">
-          <p className="menu__week">
-            Next week:
-            {box[0].menu.nextWeek.weekNumber}
-          </p>
-          <div className="menu__recipes">
-            <div className="recipe__one">
-              <p className="recipe__name">{box[0].menu.nextWeek.recipes.recipeOne.name}</p>
-              <ul className="recipe__ingredients">{box[0].menu.nextWeek.recipes.recipeOne.ingredients.map(i => (<li>{i}</li>))}</ul>
-            </div>
-          </div>
-        </div>
-        <div className="box__menu">
-          <p className="menu__week">
-            Week after:
-            {box[0].menu.weekAfter.weekNumber}
-          </p>
-          <div className="menu__recipes">
-            <div className="recipe__one">
-              <p className="recipe__name">{box[0].menu.weekAfter.recipes.recipeOne.name}</p>
-              <ul className="recipe__ingredients">{box[0].menu.weekAfter.recipes.recipeOne.ingredients.map(i => (<li>{i}</li>))}</ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </article>
+      <p>
+        for
+        {order[1]}
+      </p>
+      <p>
+        {order[1] === 2 ? box[0].price.peopleTwo : box[0].price.peopleFour}
+      </p>
+    </div>
   );
 };
 
