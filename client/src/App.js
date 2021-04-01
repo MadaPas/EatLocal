f3cdee70f508409e215a3d3e38720e1fd2f32382
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-04-01 16:11:42 +0200

    moved context

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,10 @@ import React from 'react';
 import {
   Route, Switch, useHistory,
 } from 'react-router-dom';
-import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+import { OktaAuth } from '@okta/okta-auth-js';
 import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
 import { Container } from 'semantic-ui-react';
+import { GeneralProvider } from './context/General';
 import config from './config';
 import Home from './components/Home';
 import Messages from './components/Messages';
@@ -16,33 +17,36 @@ import Box from './components/Box';
 import Checkout from './components/Checkout';
 
 const oktaAuth = new OktaAuth(config.oidc);
-
+// restoreOriginalUri={restoreOriginalUri}, toRelativeUrl
 const App = () => {
-  // const [boxId, setBox] = useState();
   const history = useHistory();
-  console.log(history, 'This is history');
-  const restoreOriginalUri = async (_oktaAuth, originalUri) => {
-    history.replace(toRelativeUrl(originalUri, window.location.origin));
-  };
+  console.log(history);
+  // const restoreOriginalUri = async (_oktaAuth, originalUri) => {
+  //   setTimeout(() => { console.log('redirecting to messages'); }, 3000);
+  //   history.push('/messages');
+  //   history.replace(toRelativeUrl(originalUri, window.location.origin));
+  // };
   return (
-    <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
-      <Navbar />
-      <Container text style={{ marginTop: '7em' }}>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-            <AllBoxes />
-          </Route>
+    <Security oktaAuth={oktaAuth}>
+      <GeneralProvider>
+        <Navbar />
+        <Container text style={{ marginTop: '7em' }}>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+              <AllBoxes />
+            </Route>
 
-          <Route path="/box/:boxId"><Box /></Route>
-          <SecureRoute path="/checkout"><Checkout /></SecureRoute>
-          <SecureRoute path="/cart"><Checkout /></SecureRoute>
-          <Route path="/login/callback" component={LoginCallback} />
-          <Route path="/boxes" component={AllBoxes} />
-          <SecureRoute path="/messages" component={Messages} />
-          <SecureRoute path="/profile" component={Profile} />
-        </Switch>
-      </Container>
+            <Route path="/box/:boxId"><Box /></Route>
+            <SecureRoute path="/checkout"><Checkout /></SecureRoute>
+            <SecureRoute path="/cart"><Checkout /></SecureRoute>
+            <Route path="/login/callback" component={LoginCallback} />
+            <Route path="/boxes" component={AllBoxes} />
+            <SecureRoute path="/messages" component={Messages} />
+            <SecureRoute path="/profile" component={Profile} />
+          </Switch>
+        </Container>
+      </GeneralProvider>
     </Security>
   );
 };
