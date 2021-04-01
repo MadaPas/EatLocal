b84aaf0e1cc633eb122394d7e0fb70d3e9130ec9
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-04-01 19:18:10 +0200

    update user router

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
 const router = express.Router();
-const { getAllUsers, getUser, registerUser } = require('../controllers/users');
+const {
+  getAllUsers, getUser, registerUser, updateUser,
+} = require('../controllers/users');
 
 router.get('/', getAllUsers);
 router.post('/user', getUser);
 router.post('/register', registerUser);
+router.put('/', updateUser);
+
 module.exports = router;
