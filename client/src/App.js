4424d06db4e2c321b49fc60d30601376b05a9216
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-04-01 19:27:09 +0200

    handle restoreOriginalUri in app

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {
   Route, Switch, useHistory,
 } from 'react-router-dom';
-import { OktaAuth } from '@okta/okta-auth-js';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
 import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
 import { Container } from 'semantic-ui-react';
 import { GeneralProvider } from './context/General';
@@ -20,17 +20,16 @@ import Success from './components/Success';
 import Fail from './components/Fail';
 
 const oktaAuth = new OktaAuth(config.oidc);
-// restoreOriginalUri={restoreOriginalUri}, toRelativeUrl
 const App = () => {
   const history = useHistory();
   console.log(history);
-  // const restoreOriginalUri = async (_oktaAuth, originalUri) => {
-  //   setTimeout(() => { console.log('redirecting to messages'); }, 3000);
-  //   history.push('/messages');
-  //   history.replace(toRelativeUrl(originalUri, window.location.origin));
-  // };
+  const restoreOriginalUri = async (_oktaAuth, originalUri) => {
+    setTimeout(() => { console.log('redirecting to messages'); }, 3000);
+    history.push('/messages');
+    history.replace(toRelativeUrl(originalUri, window.location.origin));
+  };
   return (
-    <Security oktaAuth={oktaAuth}>
+    <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
       <GeneralProvider>
         <Navbar />
         <Container text style={{ marginTop: '7em' }}>
@@ -39,7 +38,6 @@ const App = () => {
               <Home />
               <AllBoxes />
             </Route>
-
             <Route path="/box/:boxId"><Box /></Route>
             <SecureRoute path="/checkout"><Checkout /></SecureRoute>
             <SecureRoute path="/cart"><Cart /></SecureRoute>
