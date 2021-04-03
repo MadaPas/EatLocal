5a9cd7cf0b8dc43664bea58ee5c68e19369efcbe
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-04-03 12:03:05 +0200

    linter

--- server/models/boxes.js
+++ server/models/boxes.js
@@ -1,139 +1,137 @@
 const mongoose = require('mongoose');
 
-const boxSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    img: {
-      type: String,
-      required: true,
-    },
-    type: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    boxPrice: {
-      peopleTwo: {
-        people: {
-          type: Number,
-          required: true,
-        },
-        priceId: {
-          type: String,
-          required: true,
-        },
-        price: {
-          type: Number,
-          required: true,
-        },
+const boxSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  img: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  boxPrice: {
+    peopleTwo: {
+      people: {
+        type: Number,
+        required: true,
       },
-      peopleFour: {
-        people: {
-          type: Number,
-          required: true,
-        },
-        priceId: {
-          type: String,
-          required: true,
-        },
-        price: {
-          type: Number,
-          required: true,
-        },
+      priceId: {
+        type: String,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
       },
     },
-    menu: {
-      nextWeek: {
-        weekNumber: {
-          type: Number,
-          required: true,
+    peopleFour: {
+      people: {
+        type: Number,
+        required: true,
+      },
+      priceId: {
+        type: String,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+    },
+  },
+  menu: {
+    nextWeek: {
+      weekNumber: {
+        type: Number,
+        required: true,
+      },
+      recipes: {
+        recipeOne: {
+          img: {
+            type: String,
+            required: true,
+          },
+          name: {
+            type: String,
+            required: true,
+          },
+          ingredients: [String],
         },
-        recipes: {
-          recipeOne: {
-            img: {
-              type: String,
-              required: true,
-            },
-            name: {
-              type: String,
-              required: true,
-            },
-            ingredients: [String],
+        recipeTwo: {
+          img: {
+            type: String,
+            required: true,
           },
-          recipeTwo: {
-            img: {
-              type: String,
-              required: true,
-            },
-            name: {
-              type: String,
-              required: true,
-            },
-            ingredients: [String],
+          name: {
+            type: String,
+            required: true,
           },
-          recipeThree: {
-            img: {
-              type: String,
-              required: true,
-            },
-            name: {
-              type: String,
-              required: true,
-            },
-            ingredients: [String],
+          ingredients: [String],
+        },
+        recipeThree: {
+          img: {
+            type: String,
+            required: true,
           },
+          name: {
+            type: String,
+            required: true,
+          },
+          ingredients: [String],
         },
       },
-      weekAfter: {
-        weekNumber: {
-          type: Number,
-          required: true,
+    },
+    weekAfter: {
+      weekNumber: {
+        type: Number,
+        required: true,
+      },
+      recipes: {
+        recipeOne: {
+          img: {
+            type: String,
+            required: true,
+          },
+          name: {
+            type: String,
+            required: true,
+          },
+          ingredients: [String],
         },
-        recipes: {
-          recipeOne: {
-            img: {
-              type: String,
-              required: true,
-            },
-            name: {
-              type: String,
-              required: true,
-            },
-            ingredients: [String],
+        recipeTwo: {
+          img: {
+            type: String,
+            required: true,
           },
-          recipeTwo: {
-            img: {
-              type: String,
-              required: true,
-            },
-            name: {
-              type: String,
-              required: true,
-            },
-            ingredients: [String],
+          name: {
+            type: String,
+            required: true,
           },
-          recipeThree: {
-            img: {
-              type: String,
-              required: true,
-            },
-            name: {
-              type: String,
-              required: true,
-            },
-            ingredients: [String],
+          ingredients: [String],
+        },
+        recipeThree: {
+          img: {
+            type: String,
+            required: true,
           },
+          name: {
+            type: String,
+            required: true,
+          },
+          ingredients: [String],
         },
       },
     },
   },
-);
+});
 
 const Box = mongoose.model('boxes', boxSchema);
 
--- server/models/users.js
+++ server/models/users.js
@@ -1,38 +1,36 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema(
-  {
-    oktaId: {
-      type: String,
-      required: true,
-    },
-    firstName: {
-      type: String,
-      required: true,
-    },
-    lastName: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      unique: true,
-      required: true,
-    },
-    street: {
-      type: String,
-    },
-    postalCode: {
-      type: Number,
-    },
-    city: {
-      type: String,
-    },
-    stripeId: {
-      type: String,
-    },
+const userSchema = new mongoose.Schema({
+  oktaId: {
+    type: String,
+    required: true,
   },
-);
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  street: {
+    type: String,
+  },
+  postalCode: {
+    type: Number,
+  },
+  city: {
+    type: String,
+  },
+  stripeId: {
+    type: String,
+  },
+});
 
 const User = mongoose.model('users', userSchema);
 
