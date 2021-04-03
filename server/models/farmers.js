b065825814c8c4c46c0fa8680eae73a09e9688c1
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-04-03 11:59:11 +0200

    farmer model schema created

---
+++ server/models/farmers.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+
+const farmerSchema = new mongoose.Schema(
+  {
+    address: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    organic: {
+      type: Boolean,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    hostType: {
+      type: String,
+      required: true,
+    },
+    propertyArea: {
+      type: String,
+      required: true,
+    },
+    methodologies: {
+      type: String,
+      required: true,
+    },
+    practices: {
+      type: String,
+      required: true,
+    },
+    animals: {
+      type: String,
+      required: true,
+    },
+    products: {
+      type: String,
+      required: true,
+    },
+    credits: {
+      type: String,
+      required: true,
+    },
+  },
+);
+
+const Farmer = mongoose.model('farmers', farmerSchema);
+
+module.exports.Farmer = Farmer;
